e34b5be8f69467ee107865db8e36b09e8224487b
Doug Jacobsen <dmjacobsen@lbl.gov>
2018-05-29 00:29:45 -0700

    pep8 improvements

--- smwflow/hss.py
+++ smwflow/hss.py
@@ -1,17 +1,17 @@
 import os
-import sys
 import subprocess
 import errno
+import codecs
+from jinja2 import Template
 import smwflow.compare
 import smwflow.manifest
 import smwflow.search
 import smwflow.smwfile
 import smwflow.variables
-import codecs
-from jinja2 import Template
 
 MANAGED_HSS = [
-    {   'repo': 'smwconf',
+    {
+        'repo': 'smwconf',
         'name': 'blade_json.sedc',
         'smwpath': '/opt/cray/hss/default/etc/blade_json.sedc',
         'fstype': 'file',
@@ -19,8 +19,8 @@ MANAGED_HSS = [
         'mode': 0644,
         'owner': 'crayadm',
         'group': 'crayadm'
-    },
-    {   'repo': 'smwconf',
+    }, {
+        'repo': 'smwconf',
         'name': 'cab_json.sedc',
         'smwpath': '/opt/cray/hss/default/etc/cab_json.sedc',
         'fstype': 'file',
@@ -166,7 +166,7 @@ MANAGED_HSS = [
     },
 ]
 
-MANAGED_HSS_FILES = { x['name']: x for x in MANAGED_HSS }
+MANAGED_HSS_FILES = {x['name']: x for x in MANAGED_HSS}
 
 def import_data(config):
     deferred_actions = []
@@ -178,15 +178,15 @@ def import_data(config):
         repo_hss = os.path.join(repo_path, 'hss')
         try:
             os.mkdir(repo_hss, 0755)
-        except OSError, e:
-            if e.errno != errno.EEXIST:
-                raise e
+        except OSError, err:
+            if err.errno != errno.EEXIST:
+                raise err
         repo_hss = os.path.join(repo_path, 'hss', '%s_hss' % config.system)
         try:
             os.mkdir(repo_hss, 0755)
-        except OSError, e:
-            if e.errno != errno.EEXIST:
-                raise e
+        except OSError, err:
+            if err.errno != errno.EEXIST:
+                raise err
 
         manifest = smwflow.manifest.Manifest(repo_hss)
 
@@ -196,14 +196,14 @@ def import_data(config):
             if os.path.exists(item['smwpath']):
                 tgt_path = os.path.join(repo_hss, item['name'])
                 command = ['cp', item['smwpath'], tgt_path]
-                rc = subprocess.call(command)
+                retcode = subprocess.call(command)
                 manifest[item['name']] = item
 
         deferred_actions.extend(manifest.save())
         deferred_actions.append("Add/Commit HSS items in %s" % repo_path)
     return deferred_actions
 
-def _git_hss_object(config, obj, hss_vars):
+def _git_hss_object(_, obj, hss_vars):
     git_data = None
     with codecs.open(obj['fullpath'], mode='r', encoding='utf-8') as rfp:
         data = rfp.read()
@@ -211,7 +211,7 @@ def _git_hss_object(config, obj, hss_vars):
         git_data = template.render(hss_vars)
     return git_data
 
-def _smw_hss_object(config, obj):
+def _smw_hss_object(_, obj):
     if not os.path.exists(obj['smwpath']):
         return None
 
@@ -220,15 +220,15 @@ def _smw_hss_object(config, obj):
         smw_data = rfp.read()
     return smw_data
 
-def _valid_hss_object(config, obj):
+def _valid_hss_object(_, obj, name):
     if 'smwpath' not in obj:
-        print 'Skipping git hss file %s since it does not have an smwpath in the manifest' % key
+        print 'Skipping git hss file %s since it does not have an smwpath in the manifest' % name
         return False
     return True
 
-def _verify_hss_object(config, obj, git_data, smw_data):
+def _verify_hss_object(config, obj, name, git_data, smw_data):
     if not git_data or not smw_data:
-        print 'Failed to read git or smw data for hss file %s (smw: %s)' % (key, obj['smwpath'])
+        print 'Failed to read git or smw data for hss file %s (smw: %s)' % (name, obj['smwpath'])
         return None
     issues = smwflow.compare.basic_compare(config, obj, git_data, smw_data)
     return issues
@@ -240,14 +240,14 @@ def verify_data(config):
     hss_vars = smwflow.variables.read_vars(config, 'hss', 'vars', None, config.global_vars)
     for key in objs:
         obj = objs[key]
-        if not _valid_hss_object(config, obj):
+        if not _valid_hss_object(config, obj, key):
             continue
 
         git_data = _git_hss_object(config, obj, hss_vars)
         smw_data = _smw_hss_object(config, obj)
 
-        issues = _verify_hss_object(config, obj, git_data, smw_data)
-        if type(issues) is list and len(issues) > 0:
+        issues = _verify_hss_object(config, obj, key, git_data, smw_data)
+        if isinstance(issues, list) and issues:
             print "DIFFERENCES FOUND IN %s" % obj['name']
             for item in issues:
                 print item
@@ -264,13 +264,13 @@ def update_data(config):
 
     for key in objs:
         obj = objs[key]
-        if not _valid_hss_object(config, obj):
+        if not _valid_hss_object(config, obj, key):
             continue
         git_data = _git_hss_object(config, obj, hss_vars)
         smw_data = _smw_hss_object(config, obj)
 
-        issues = _verify_hss_object(config, obj, git_data, smw_data)
-        if issues is None or len(issues) > 0:
+        issues = _verify_hss_object(config, obj, key, git_data, smw_data)
+        if issues is None or issues:
             print "Updating HSS component %s in %s" % (obj['name'], obj['smwpath'])
             with codecs.open(obj['smwpath'], mode='w', encoding='utf-8') as wfp:
                 wfp.write(git_data)
