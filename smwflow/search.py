25e9dc04fc453ed41e7409b3202cb07e0b873f79
Doug Jacobsen <dmjacobsen@lbl.gov>
2018-05-29 00:29:45 -0700

    pep8 improvements

--- smwflow/search.py
+++ smwflow/search.py
@@ -1,8 +1,7 @@
 import os
-import sys
 import smwflow.manifest
 
-def gen_paths(config, maintype, objtype, subtype=None, repos=('smwconf','secured'), system=None):
+def gen_paths(config, maintype, objtype, subtype=None, repos=('smwconf', 'secured'), system=None):
     """
     Generate search paths where objects of given maintype/objtype/subtype may be found.
 
@@ -35,11 +34,13 @@ def gen_paths(config, maintype, objtype, subtype=None, repos=('smwconf','secured
 
     return [x for x in paths if os.path.exists(x) and os.access(x, os.R_OK)]
 
-def get_objects(config, maintype, objtype, subtype=None, extra_obj_parameters={}, repos=('smwconf','secured'), system=None):
+def get_objects(config, maintype, objtype, subtype=None, extra_obj_parameters=None, repos=('smwconf', 'secured'), system=None):
     """
     Get a dictionary of objects, annorated with the most relevant manifest
     entry, if it exists.
     """
+    if not extra_obj_parameters:
+        extra_obj_parameters = {}
     if not system:
         system = config.system
     output = {}
@@ -49,17 +50,16 @@ def get_objects(config, maintype, objtype, subtype=None, extra_obj_parameters={}
         rpath = os.path.realpath(path)
         start_idx = len(rpath) + 1
 
-        for (dirpath, dirnames, filenames) in os.walk(rpath):
+        for (dirpath, _, filenames) in os.walk(rpath):
             for filename in filenames:
                 if filename == '.smwflow.manifest.yaml':
                     continue
                 filename = os.path.join(dirpath, filename)[start_idx:]
-                output[filename] = { 'fullpath': os.path.join(dirpath, filename) }
+                output[filename] = {'fullpath': os.path.join(dirpath, filename)}
                 if filename in manifest:
                     for key in manifest[filename]:
                         output[filename][key] = manifest[filename][key]
                 if filename in extra_obj_parameters:
                     for key in extra_obj_parameters[filename]:
                         output[filename][key] = extra_obj_parameters[filename][key]
-         
     return output
